d0281c12128b3c9b80dfe5aeb988b08aee1678a9
“ALEX <alexgtr84@gmail.com>
2019-02-12 13:22:16 +0100

    Typescript lengthy setup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import 'reflect-metadata'
-import {createKoaServer} from "routing-controllers"
-import Controller from "./controller"
-
-const port = process.env.PORT || 4000
+import {createKoaServer} from 'routing-controllers'
+import PageController from './pages/controller'
+import setupDb from './db'
 
 const app = createKoaServer({
-   controllers: [Controller]
+  controllers: [
+    PageController
+  ]
 })
 
-app.listen(port, () => console.log(`Listening on port ${port}`))
+setupDb()
+  .then(_ =>
+    app.listen(4000, () => console.log('Listening on port 4000'))
+  )
+  .catch(err => console.error(err))
+
+// app.listen(4000, () => console.log('Listening on port 4000'))
