fd086b7e519551f43cad08c9d3677cd321e6a19c
“ALEX <alexgtr84@gmail.com>
2019-02-12 16:36:18 +0100

    Adds port variable

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import LoginController from './logins/controller'
 import setupDb from './db'
 import {verify} from './jwt'
 
+const port = process.env.PORT || 4000
+
 const app = createKoaServer({
   controllers: [
     PageController,
@@ -26,7 +28,7 @@ const app = createKoaServer({
 
 setupDb()
   .then(_ =>
-    app.listen(4000, () => console.log('Listening on port 4000'))
+    app.listen(port, () => console.log('Listening on port 4000'))
   )
   .catch(err => console.error(err))
 
